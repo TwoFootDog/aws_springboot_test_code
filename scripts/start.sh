5e6c851646ee6fa096687aaff490aead12c12f7d
two dog <ekfrl2815@gmail.com>
2020-02-14 23:49:57 +0900

    Bbb

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,6 +7,18 @@ echo "> build 파일 복사" >> /home/ec2-user/deploy.log
 DEPLOY_PATH=/home/ec2-user/
 cp $BUILD_PATH $DEPLOY_PATH
 
+echo "> 현재 실행중인 애플리케이션 pid 확인"
+CURRENT_PID=$(pgrep -f $APPLICATION_JAR_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
 APPLICATION_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> $APPLICATION_JAR 배포"    >> /home/ec2-user/deploy.log
 nohup java -jar $APPLICATION_JAR >> /home/ec2-user/deploy.log &
