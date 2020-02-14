b167117151a2f848a8050e4c503a3fefb8116505
two dog <ekfrl2815@gmail.com>
2020-02-14 21:06:02 +0900

    hh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-JAR_FILE=$(basename ./*.jar)
-echo $JAR_FILE
-nohup java -jar $JAR_FILE >./java.log &
+BUILD_PATH=$(ls /home/ec2-user/build/*.jar)
+JAR_NAME=$(basename $BUILD_PATH)
+echo "> build 파일명: $JAR_NAME" >> deploy.log
+
+echo "> build 파일 복사" >> deploy.log
+DEPLOY_PATH=/home/ec2-user/
+cp $BUILD_PATH $DEPLOY_PATH
+
+APPLICATION_JAR=$DEPLOY_PATH$JAR_NAME
+echo "> $APPLICATION_JAR 배포"    >> deploy.log
+nohup java -jar $APPLICATION_JAR >> deploy.log
