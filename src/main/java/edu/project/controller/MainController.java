79e9d8eccd8d487ddc9834fc72ccd4266796d34e
two dog <ekfrl2815@gmail.com>
2020-02-15 00:12:06 +0900

    bbb

--- src/main/java/edu/project/controller/MainController.java
+++ src/main/java/edu/project/controller/MainController.java
@@ -1,13 +1,19 @@
 package edu.project.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+    private Logger logger = LoggerFactory.getLogger(MainController.class);
 
     @GetMapping("/")
     public String rootFunction() {
+        logger.info("---------------------------");
+        logger.info("TEST");
+        logger.info("---------------------------");
         return "SUCCESS";
     }
 }
