0c2966d67fdba5e0a54e09c2816d662eacf085fb
two dog <ekfrl2815@gmail.com>
2020-02-17 20:51:29 +0900

    bb

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-BUILD_PATH=$(ls /home/ec2-user/build/*.jar)
-JAR_NAME=$(basename $BUILD_PATH)
+BUILD_JAR=$(ls /home/ec2-user/build/*.jar)
+JAR_NAME=$(basename $BUILD_JAR)
 echo "> build 파일명: $JAR_NAME" >> /home/ec2-user/deploy.log
 
 echo "> build 파일 복사" >> /home/ec2-user/deploy.log
 DEPLOY_PATH=/home/ec2-user/
-cp $BUILD_PATH $DEPLOY_PATH
+cp $BUILD_JAR $DEPLOY_PATH
 
 echo "> 현재 실행중인 애플리케이션 pid 확인"
 CURRENT_PID=$(pgrep -f $JAR_NAME)
@@ -19,6 +19,6 @@ else
   sleep 5
 fi
 
-APPLICATION_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> $APPLICATION_JAR 배포"    >> /home/ec2-user/deploy.log
-nohup java -jar $APPLICATION_JAR >> /home/ec2-user/deploy.log &
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/deploy.log
+nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log &
